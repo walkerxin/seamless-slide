52963bace52616289bcc886ef0b680ade6fb7905
walkerxin <circle_Web@163.com>
2018-12-06 23:10:38 +0800

    实现无缝轮播（抽象出来）

--- 20181205/main.js
+++ 20181205/main.js
@@ -1,25 +1,26 @@
-setTimeout(function() {
-    // .css() 相当于改变style属性，为什么不叫.style()
-    $('.images>img:nth-child(1)').css({ transform: 'translateX(-100%)' })
+$('.images>img:nth-child(1)').addClass('current')
+$('.images>img:nth-child(1)').siblings().addClass('enter')
+
+let n = 1
+setInterval(() => {
+
+    $('.images>img:nth-child(' + getNum(n) + ')').removeClass('current').addClass('leave')
         .one('transitionend', function(e) {
-            $(e.currentTarget).addClass('right').css({ transform: 'translateX(0)' })
+            $(e.currentTarget).removeClass('leave').addClass('enter')
         })
-    $('.images>img:nth-child(2)').css({ transform: 'translateX(-100%)' })
+    $('.images>img:nth-child(' + getNum(n+1) + ')').removeClass('enter').addClass('current')
+    
+    n += 1
+
 }, 2000)
 
-setTimeout(function() {
-    $('.images>img:nth-child(2)').css({ transform: 'translateX(-200%)' })
-        .one('transitionend', function(e) {
-            $(e.currentTarget).addClass('right').css({ transform: 'none' })
-        })
-    $('.images>img:nth-child(3)').css({ transform: 'translateX(-100%)' })
-}, 4000)
-
-setTimeout(function() {
-    $('.images>img:nth-child(3)').css({ transform: 'translateX(-200%)' })
-        .one('transitionend', function(e) {
-            $(e.currentTarget).addClass('right').css({ transform: 'none' })
-        })
-    $('.images>img:nth-child(1)').css({ transform: 'translateX(-100%)', 'z-index': '1'})
-    // 此处第一张到右边需加z-index，否则因为文档流最后一张浮在上面，会看到最后一张飘过
-}, 6000)
+function getNum(num) {
+    let size = $('.images').children().length
+    if(num > size) {
+        num = num % size
+    }
+    if(num == 0) {
+        num = size
+    }
+    return num
+}
